080b56c9e6ed5ae33420ad2b8bf71c33b279591c
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-01-23 12:27:13 -0500

    Comment out markdown

--- markdown-mmm.el
+++ markdown-mmm.el
@@ -1,20 +1,20 @@
-(require 'mmm-auto)
-(setq mmm-global-mode 'maybe)
+;; (require 'mmm-auto)
+;; (setq mmm-global-mode 'maybe)
 
-(defun my-mmm-markdown-auto-class (lang &optional submode)
-  "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
-If SUBMODE is not provided, use `LANG-mode' by default."
-  (let ((class (intern (concat "markdown-" lang)))
-        (submode (or submode (intern (concat lang "-mode"))))
-        (front (concat "^```" lang "[\n\r]+"))
-        (back "^```"))
-    (mmm-add-classes (list (list class :submode submode :front front :back back)))
-    (mmm-add-mode-ext-class 'markdown-mode nil class)
-    (mmm-add-mode-ext-class 'gfm-mode nil class)))
+;; (defun my-mmm-markdown-auto-class (lang &optional submode)
+;;   "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
+;; If SUBMODE is not provided, use `LANG-mode' by default."
+;;   (let ((class (intern (concat "markdown-" lang)))
+;;         (submode (or submode (intern (concat lang "-mode"))))
+;;         (front (concat "^```" lang "[\n\r]+"))
+;;         (back "^```"))
+;;     (mmm-add-classes (list (list class :submode submode :front front :back back)))
+;;     (mmm-add-mode-ext-class 'markdown-mode nil class)
+;;     (mmm-add-mode-ext-class 'gfm-mode nil class)))
 
-;; Mode names that derive directly from the language name
-(mapc 'my-mmm-markdown-auto-class
-      '("awk" "bibtex" "c" "cpp" "css" "html" "latex" "lisp" "makefile"
-        "markdown" "python" "r" "ruby" "sql" "stata" "xml" "haskell"))
+;; ;; Mode names that derive directly from the language name
+;; (mapc 'my-mmm-markdown-auto-class
+;;       '("awk" "bibtex" "c" "cpp" "css" "html" "latex" "lisp" "makefile"
+;;         "markdown" "python" "r" "ruby" "sql" "stata" "xml" "haskell"))
 
-(setq mmm-parse-when-idle 't)
+;; (setq mmm-parse-when-idle 't)
